0761330f52da1c36c7833ee60bae3aa918b2bea1
Parker Hemming <parker@hemm.ing>
2026-07-31 21:06:31 -0600

    prod ready

--- service/s3-config.js
+++ service/s3-config.js
@@ -6,7 +6,7 @@ const s3Client = new S3Client({
 	region: "us-east-1",
 	credentials: fromIni({
 		filepath: path.join(__dirname, "aws-credentials.ini"),
-		profile: "local",
+		profile: "production",
 	}),
 });
 
